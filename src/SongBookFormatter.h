4d6224185663a23753acd0405ab8b1740f997e42
petrcechura <petr.cechura@tutanota.com>
2025-09-30 16:18:16 +0200

    added stub SongBookFormatter.h

---
+++ src/SongBookFormatter.h
@@ -0,0 +1,12 @@
+
+
+/** This virtual class shall provide an interface for a Gather and Latex task.
+    - takes chords in plain text as an argument and calls an AI to parse to **implementation-depedent** format (bard, latex)
+    - provides set of methods to convert this text to a pdf
+    - creates entire songbook
+    
+    - A visitor pattern*/
+class SongBookFormatter
+{
+
+}
